2e3a55e605e0f765d1a06a2bc709eb00bc461550
Shunze Lin <4A490101@stust.edu.tw>
2021-03-06 19:58:21 +0800

    feat:Using server side rendering to get styles and put into style tags.

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -1,6 +1,19 @@
 import Document, { Html, Head, NextScript, Main } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 class MyDocument extends Document {
+	static getInitialProps({ renderPage }) {
+		const sheet = new ServerStyleSheet();
+		// Go through every component that needs be rendered,
+		// see if it has styled components.
+		const page = renderPage((App) => (props) =>
+			sheet.collectStyles(<App {...props} />)
+		);
+		// Gathering all styles into tags.
+		const styleTags = sheet.getStyleElement();
+		return { ...page, styleTags };
+	}
+
 	render() {
 		return (
 			<Html lang="en-CA">
