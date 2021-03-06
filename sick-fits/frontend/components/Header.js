021b49ac32a1026b7f238f82b8c57a62c0cb3943
Shunze Lin <4A490101@stust.edu.tw>
2021-03-06 19:19:38 +0800

    feat:Using styled components with CSS props to style Header component

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,12 +1,50 @@
 import Link from "next/link";
 import Nav from "./Nav";
+import styled from "styled-components/macro";
+
 const Header = () => {
 	return (
 		<header>
-			<div className="bar">
-				<Link href="/">Sick fits</Link>
+			<div
+				css={`
+					border-bottom: solid 10px var(--color-black, black);
+					display: grid;
+					grid-template-columns: auto 1fr;
+					justify-content: space-between;
+					align-items: center;
+				`}
+			>
+				<h1
+					css={`
+						background: #f00;
+						font-size: 4rem;
+						margin-left: 2rem;
+						position: relative;
+						z-index: 2;
+						transform: skew(-7deg);
+					`}
+				>
+					<Link href="/">
+						<a
+							css={`
+								color: #fff;
+								text-decoration: none;
+								text-transform: uppercase;
+								padding: 0.5rem 1rem;
+							`}
+						>
+							Sick fits
+						</a>
+					</Link>
+				</h1>
 			</div>
-			<div className="sub-bar">
+			<div
+				css={`
+					display: grid;
+					grid-template-columns: 1fr auto;
+					border-bottom: solid 1px var(--color-black, black);
+				`}
+			>
 				<p>Search</p>
 			</div>
 			<Nav />
