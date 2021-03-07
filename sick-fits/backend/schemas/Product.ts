752466f264818964cae33b8e7bb2213f64d18da5
Shunze Lin <4A490101@stust.edu.tw>
2021-03-07 09:49:53 +0800

    feat: Creating relationship between Product and ProductImage

--- sick-fits/backend/schemas/Product.ts
+++ sick-fits/backend/schemas/Product.ts
@@ -1,10 +1,19 @@
 import { list } from "@keystone-next/keystone/schema";
-import { text, select, integer } from "@keystone-next/fields";
+import { text, select, integer, relationship } from "@keystone-next/fields";
 
 const Product = list({
 	fields: {
 		name: text({ isRequired: true }),
 		description: text({ ui: { displayMode: "textarea" } }),
+		photo: relationship({
+			ref: "ProductImage.product",
+			ui: {
+				displayMode: "cards",
+				cardFields: ["image", "altText"],
+				inlineCreate: { fields: ["image", "altText"] },
+				inlineEdit: { fields: ["image", "altText"] },
+			},
+		}),
 		status: select({
 			options: [
 				{ label: "Draft", value: "DRAFT" },
